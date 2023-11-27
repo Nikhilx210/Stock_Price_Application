9ce1ec06810ff302c07a47093066cfea9b9126d8
jai <jaigaur.imp210@gmail.com>
2023-11-27 16:13:31 +0530

    SPP-4 frontend2

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { ChakraProvider } from '@chakra-ui/react'
+import Stock_Context from './Context/Stock_Context';
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Stock_Context>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </Stock_Context>
+  </React.StrictMode>
+);
 
 
