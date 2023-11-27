0c2417aa6d2864c82a2afff1e0cb41142963094e
jai <jaigaur.imp210@gmail.com>
2023-11-27 16:14:02 +0530

    SPP-4 front_last

--- frontend/src/Context/Stock_Context.js
+++ frontend/src/Context/Stock_Context.js
@@ -1,1 +1,20 @@
 
+import React, { createContext, useState,useContext } from 'react'
+
+const Stock=createContext();
+const Stock_Context = ({children}) => {
+    const[stock_price,setstock_price]=useState([]);
+    const[history_loss,sethistory_loss]=useState([]);
+    const[history_val,sethistory_val]=useState([]);
+    const[StockCode,setStockCode]=useState('');
+  return (
+    <Stock.Provider value={[stock_price,setstock_price,StockCode,setStockCode,history_loss,history_val,sethistory_loss,sethistory_val]}>
+      {children}
+    </Stock.Provider>
+  )
+}
+export const useStock = () => {
+    return useContext(Stock);
+  };
+
+export default Stock_Context
